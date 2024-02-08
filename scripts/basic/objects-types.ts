5d23b5b7b9e0c3b7f3235cf3f91c4f4712738002
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-02-08 18:03:40 -0300

    add objects types

---
+++ scripts/basic/objects-types.ts
@@ -0,0 +1,14 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "Filho",
+//   age: 30,
+// };
+
+const person = {
+  name: "Maximilan",
+  age: 30,
+};
+
+console.log(person.name);
