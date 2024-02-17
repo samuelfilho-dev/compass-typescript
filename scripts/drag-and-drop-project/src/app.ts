0d3078082e1d2224e5b0016c34e71e75f6e01a2f
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-02-17 19:47:58 -0300

    add new Dependencies

--- scripts/drag-and-drop-project/src/app.ts
+++ scripts/drag-and-drop-project/src/app.ts
@@ -1,3 +1,36 @@
+// Validação usando decorators da depedencia 'class-validator'
+
+// import { IsNotEmpty, IsNumber, IsPositive } from "class-validator";
+// import { validate } from "class-validator";
+
+// class Product {
+//   @IsNotEmpty()
+//   title: string;
+
+//   @IsNumber()
+//   @IsPositive()
+//   price: number;
+
+//   constructor(title: string, price: number) {
+//     this.title = title;
+//     this.price = price;
+//   }
+
+//   getInfo() {
+//     return [this.title, `$${this.price}`];
+//   }
+// }
+
+// const newProd = new Product("", 5.99);
+// validate(newProd).then((erros) => {
+//   if (erros.length > 0) {
+//     console.log("VALIDATION ERROS!");
+//     console.log(erros);
+//   } else {
+//     console.log(newProd.getInfo());
+//   }
+// });
+
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
 
