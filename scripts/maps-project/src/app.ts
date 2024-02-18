8367f66a08ecbc50e5dadc9f174fd3189cc393b6
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-02-17 21:10:48 -0300

    add maps project

---
+++ scripts/maps-project/src/app.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg";
+
+type GoogleGeoCondingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeoCondingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      console.log(response);
+      if (response.data.status !== "OK") {
+        throw new Error("Could Not Fetch Location");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+
+      const map = new google.maps.Map(document.getElementById("map")!, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 16,
+      });
+
+      new google.maps.Marker({ position: coordinates, map: map });
+
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
